56b3477de39f461627a3fbe05d1c924f6e8c787e
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-13 00:16:59 -0800

    Structure next.js output

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { Press_Start_2P } from 'next/font/google';
+
+const pressStart = Press_Start_2P({ weight: '400', subsets: ['latin'] });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<main className={pressStart.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
 
-export default MyApp
+export default MyApp;
