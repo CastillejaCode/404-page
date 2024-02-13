6d095d6815f2979f4ce7f59a44a4a7de3a4e5a49
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-13 00:40:11 -0800

    Add multiple slots

---
+++ pages/SlotMachine/Slot.tsx
@@ -0,0 +1,24 @@
+import { animate } from 'motion';
+import { useEffect, useRef } from 'react';
+
+type Props = {
+	array: number[];
+};
+
+export default function Slot({ array }: Props) {
+	const ref = useRef<HTMLDivElement>();
+
+	useEffect(() => {
+		animate(
+			(progress) =>
+				(ref.current.innerHTML = String(
+					array.at(Math.round(progress * array.length - 1))
+				)),
+			{
+				duration: 2,
+				easing: 'ease-out',
+			}
+		);
+	}, [array]);
+	return <div ref={ref} className='container'></div>;
+}
--- pages/SlotMachine/index.tsx
+++ pages/SlotMachine/index.tsx
@@ -1,37 +1,35 @@
-import { animate } from 'motion';
-import { useRef } from 'react';
+import { FormEvent, useState } from 'react';
+import Slot from './Slot';
 
 export default function SlotMachine() {
-	const ref = useRef<HTMLDivElement>();
+	const [arr, setArr] = useState(randomize());
 
-	function getRandomIntInclusive(min, max) {
-		const minCeiled = Math.ceil(min);
-		const maxFloored = Math.floor(max);
-		return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-	}
-
-	function createRandomArray(start, end, length: number) {
-		return Array.from({ length }, () => getRandomIntInclusive(start, end));
-	}
-
-	function randomize() {
-		const array = createRandomArray(1, 9, 10);
-		animate(
-			(progress) =>
-				(ref.current.innerHTML = String(
-					array.at(Math.round(progress * array.length - 1))
-				)),
-			{
-				duration: 2,
-				easing: 'ease-out',
-			}
-		);
+	function handleSubmit(e: FormEvent) {
+		e.preventDefault();
+		setArr(randomize());
 	}
 
 	return (
-		<div>
-			<div ref={ref} className='container'></div>
-			<button onClick={randomize}>Reset</button>
-		</div>
+		<form onSubmit={handleSubmit}>
+			<div className='slots'>
+				{arr.map((e, i) => {
+					return <Slot key={i} array={e} />;
+				})}
+			</div>
+			<button>Reset</button>
+		</form>
 	);
+}
+function getRandomIntInclusive(min, max) {
+	const minCeiled = Math.ceil(min);
+	const maxFloored = Math.floor(max);
+	return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+function createRandomArray(start, end, length: number) {
+	return Array.from({ length }, () => getRandomIntInclusive(start, end));
+}
+
+function randomize() {
+	return Array.from({ length: 3 }, (e) => createRandomArray(1, 9, 10));
 }
