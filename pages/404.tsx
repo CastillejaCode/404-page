4b47473be8ab3f0d0ab7b5807d3ae4fa452f01f5
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-16 17:54:31 -0800

    Add page to allow navigation to error

---
+++ pages/404.tsx
@@ -0,0 +1,5 @@
+import SlotMachine from './SlotMachine';
+
+export default function Custom404() {
+	return <SlotMachine />;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
-import SlotMachine from './SlotMachine';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
-export default function Custom404() {
-	return <SlotMachine />;
+const Button = styled.button``;
+
+export default function Home() {
+	const router = useRouter();
+
+	function navigateToError() {
+		router.push('doesnt-exist');
+	}
+	return <Button onClick={navigateToError}>Error</Button>;
 }
