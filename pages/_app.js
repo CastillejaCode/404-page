5ba2bb367b87f44a7ada37bc96043a55d290ec96
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-16 16:27:11 -0800

    Add font

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 import styled from 'styled-components';
-// import { Press_Start_2P } from 'next/font';
-
-// const pressStart = Press_Start_2P({ weight: '400', subsets: ['latin'] });
 
 const Main = styled.main`
 	display: flex;
