95aa222893d55939bfbf96b70adb2f98d87eb7c3
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-20 13:13:52 -0800

    Refactor utility functions

--- pages/components/SlotMachine/index.tsx
+++ pages/components/SlotMachine/index.tsx
@@ -1,8 +1,12 @@
 import { FormEvent, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import Coins from '../Coins';
-import Slots from '../../Slots';
-import { convertToNumberArray, createRandomDurations } from '../../utils';
+import Slots from '../Slots';
+import {
+	convertToNumberArray,
+	createRandomDurations,
+	getRandomIntInclusive,
+} from '../../utils';
 
 const Form = styled.form`
 	display: flex;
@@ -89,17 +93,7 @@ export default function SlotMachine({ spinLimit }: Props) {
 	);
 }
 
-// Shamelessly stolen from MDN
-function getRandomIntInclusive(min: number, max: number) {
-	const minCeiled = Math.ceil(min);
-	const maxFloored = Math.floor(max);
-	return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-}
-
-function createRandomArray({ min = 0, max = 9, length = 9 } = {}) {
-	return Array.from({ length }, () => getRandomIntInclusive(min, max));
-}
-
+// Create an array of arrays of random numbers for each slot
 function createRandomArrays({ riggedNumber = 0, length = 3 } = {}) {
 	let arr = Array.from({ length }, createRandomArray);
 
@@ -112,4 +106,8 @@ function createRandomArrays({ riggedNumber = 0, length = 3 } = {}) {
 	}
 
 	return arr;
+}
+
+function createRandomArray({ min = 0, max = 9, length = 9 } = {}) {
+	return Array.from({ length }, () => getRandomIntInclusive(min, max));
 }
--- pages/utils/index.ts
+++ pages/utils/index.ts
@@ -7,3 +7,10 @@ export function createRandomDurations({ min = 1, max = 3, length = 3 } = {}) {
 		Math.round((Math.random() * (max - min) + min) * 1000)
 	);
 }
+
+// Shamelessly stolen from MDN
+export function getRandomIntInclusive(min: number, max: number) {
+	const minCeiled = Math.ceil(min);
+	const maxFloored = Math.floor(max);
+	return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
