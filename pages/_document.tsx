ad39178fe4afcb74ebf7160a3ed6878bc64c3edb
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-02-22 13:05:06 -0800

    Change title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,7 +21,7 @@ export default class MyDocument extends Document {
 										name='viewport'
 										content='width=device-width, initial-scale=1.0'
 									/>
-									<title>404</title>
+									<title>Not Found</title>
 									<link
 										rel='apple-touch-icon'
 										sizes='180x180'
